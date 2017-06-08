2ab1b0716d52330b8185d29559aa177dd2fa5e4c
lavac <lavanyacs743@gmail.com>
2017-06-08 23:16:53 +0530

    made some mistakes to check with the gradle

--- src/main/org/tw/java/Hello.java
+++ src/main/org/tw/java/Hello.java
@@ -1,7 +1,7 @@
 package org.tw.java;
 public class Hello {
     public static void main(String args[]) {
-        System.out.print("hello");
+        System.out.print("hello")
     }
 
 }
--- src/test/org/tw/java/HelloTest.java
+++ src/test/org/tw/java/HelloTest.java
@@ -1,9 +1,12 @@
 package org.tw.java;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class HelloTest {
     @Test
     public void shouldReturnHelloWord() {
         Hello hello = new Hello();
-
+        assertTrue("heelo".equals("rh"));
     }
 }
